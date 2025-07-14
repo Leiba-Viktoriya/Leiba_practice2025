37bf21d8d32d2142af0bf0625931665f719518d6
Лейба Виктория <victuuu0@gmail.com>
2025-07-14 12:42:24 +0600

    task13

---
+++ task13/Models/Subject.cs
@@ -0,0 +1,7 @@
+namespace practice2025.Task13.Models;
+
+public class Subject
+{
+    public string Name { get; set; }
+    public int Grade { get; set; }
+}
---
+++ task13tests/StudentSerializationTests.cs
@@ -0,0 +1,64 @@
+using System;
+using System.IO;
+using Xunit;
+using practice2025.Task13;
+using practice2025.Task13.Models;
+
+namespace task13tests;
+
+public class StudentSerializationTests
+{
+    private readonly Student sample = new()
+    {
+        FirstName = "Ivan",
+        LastName = "Petrov",
+        BirthDate = new DateTime(2000, 5, 15),
+        Grades = new List<Subject>
+        {
+            new Subject { Name = "Math", Grade = 5 },
+            new Subject { Name = "Physics", Grade = 4 }
+        }
+    };
+
+    [Fact]
+    public void SerializeStudent_IncludesPropertiesAndDateFormat()
+    {
+        var json = JsonHelper.SerializeStudent(sample);
+        Assert.Contains("\"FirstName\": \"Ivan\"", json);
+        Assert.Contains("\"BirthDate\": \"2000-05-15\"", json);
+    }
+
+    [Fact]
+    public void DeserializeStudent_ValidJson_ReturnsStudent()
+    {
+        var json = JsonHelper.SerializeStudent(sample);
+        var student = JsonHelper.DeserializeStudent(json);
+        Assert.Equal(sample.FirstName, student.FirstName);
+        Assert.Equal(sample.LastName, student.LastName);
+        Assert.Equal(sample.BirthDate, student.BirthDate);
+    }
+
+    [Fact]
+    public void DeserializeStudent_InvalidData_Throws()
+    {
+        var invalid = "{\"FirstName\":\"\",\"LastName\":\"\",\"BirthDate\":\"2000-05-15\",\"Grades\":null}";
+        Assert.Throws<InvalidDataException>(() => JsonHelper.DeserializeStudent(invalid));
+    }
+
+    [Fact]
+    public void SaveAndLoad_FileOperationsWork()
+    {
+        var path = Path.GetTempFileName();
+        try
+        {
+            var json = JsonHelper.SerializeStudent(sample);
+            JsonHelper.SaveToFile(path, json);
+            var loaded = JsonHelper.LoadFromFile(path);
+            Assert.Equal(json, loaded);
+        }
+        finally
+        {
+            File.Delete(path);
+        }
+    }
+}
